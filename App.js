efa0f20cc77ccb579dedea98bf4917d0bd4891de
boceksoft_ofis <huseyin.topuz35@gmail.com>
2023-07-28 10:17:30 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const FonImage = require("./assets/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageViewer FonImage={FonImage} />
+      <View style={styles.footerContainer}>
+        <Button label="Use this photo" />
+        <Button label="Choose this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#23282D",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
 });
